4ca2001efa6c904bace6a573d99327b713b7f081
AAlben <lichenguang123@hotmail.com>
2016-04-27 17:07:52 +0800

    1.重新编写后台商品新增页面、编辑页面

---
+++ StaticContent/Admin/js/commOpe.js
@@ -0,0 +1,206 @@
+﻿//初始化上传按钮
+function InitUploadControl() {
+    //Config
+    var vConfig = UploadInfoConfig('multi');
+
+    //Function
+    vConfig.onUploadSuccess = function (file, data, respose) {
+        var obj = jQuery.parseJSON(data); //把返回的数据序列化为Obj对象
+
+        if (obj.ret) {
+            UploadImgAppend(obj.FilePath)
+        }
+        else {
+            alert(obj.message);
+        }
+    }
+
+    //Init
+    $("#img_add").uploadify(vConfig);
+}
+
+//动态加载图片
+function UploadImgAppend(vPath) {
+    //Item
+    var vItem = $('#uPhotoList');
+
+    var vHtml = '';
+
+    //hidden
+    var vLiPhotoIndex = $('#hLPhotoIndex');
+    var vIndex = parseInt(vLiPhotoIndex.val());
+    vLiPhotoIndex.val(vIndex + 1);
+
+    var vTxt = '';
+
+    switch (vIndex) {
+        case 0:
+            vTxt = '商品封面图';
+            break;
+
+        default:
+            vTxt = '商品滚动图';
+            break;
+    }
+
+    vHtml += TxtFormat('<li uPath="{0}">', [vPath]);
+    vHtml += '<figure class="uk-overlay">';
+    vHtml += TxtFormat('<img src="/{0}" />', [vPath]);
+    vHtml += '<figcaption class="uk-overlay-panel uk-overlay-background uk-overlay-bottom">';
+    vHtml += TxtFormat('<h2>{0}</h2>', [vTxt]);
+    //vHtml += TxtFormat('<p class="uk-grid"><a class="uk-width-1-1 font-white" onclick="PhotoDelete();">删除</a></p>', [vLiPhotoIndex, vLiPhotoIndex]);
+    vHtml += '<p class="uk-grid"><a class="uk-width-1-2 uk-text-center font-white txt-nowrap" onclick="SetInfoPhoto();">设为封面</a><a class="uk-width-1-2 uk-text-center font-white txt-nowrap" onclick="PhotoDelete();">删除</a></p>';
+    vHtml += '</figcaption>';
+    vHtml += '</figure>';
+    vHtml += '</li>';
+
+    vItem.append(vHtml);
+}
+
+//上传的图片，指定一个为封面图
+function SetInfoPhoto() {
+    //Init
+    var vEvent = event.currentTarget;
+    var vUl = $('#uPhotoList');
+
+    //筛选
+    var vLi = $(vEvent).parentsUntil('li');
+    vLi = vLi.parent('li');
+
+    $('li:first h2', vUl).text('商品滚动图');
+
+    //变动
+    $('h2', vLi).text('商品封面图');
+    vUl.prepend(vLi);
+}
+
+//删除指定上传的图片
+function PhotoDelete() {
+    //Init
+    var vEvent = event.currentTarget;
+
+    $(vEvent).parentsUntil('li').parent('li').remove();
+}
+
+//删除商品规格：单行
+function btnDeleteOption() {
+    //Init
+    var vEvent = event.currentTarget;
+
+    var vLi = $(vEvent).parentsUntil('li').parent('li');
+
+    if (vLi.parent('ul').find('li').length <= 2) {
+        $('#hCommOptionFlag').val('0');
+    }
+
+    vLi.remove();
+}
+
+//增加一行商品规格：单行
+function btnAddOption() {
+    $('#hCommOptionFlag').val('1');//更改标识
+
+    var vHtml = '';
+
+    vHtml += '<li class="sku_li" uId="undefined">';
+    vHtml += '<div class="sku_item_b">';
+    vHtml += '<input type="text" class="sku_item_m_input input input_y block wrap c_txt">';
+    vHtml += '</div>';
+    vHtml += '<div class="sku_item_b"><input type="text" class="sku_item_b_input input input_y block wrap c_txt"></div>';
+    vHtml += '<div class="sku_item_d"><a class="del_sku red_txt pointer block" onclick="btnDeleteOption();">删除</a></div>';
+    vHtml += '</li>';
+
+    $('#sku_ul').append(vHtml);
+}
+
+//商品规格初始化
+function InitOptionData(vOptionList) {
+
+    var vHtml = '';
+
+    $.each(vOptionList, function (index, item) {
+        vHtml += TxtFormat('<li class="sku_li" uId="{0}">', [item.ID]);
+        vHtml += '<div class="sku_item_b">';
+        vHtml += TxtFormat('<input type="text" class="sku_item_m_input input input_y block wrap c_txt" value="{0}">', [item.Name]);
+        vHtml += '</div>';
+        vHtml += TxtFormat('<div class="sku_item_b"><input type="text" class="sku_item_b_input input input_y block wrap c_txt" value="{0}"></div>', [item.Price]);
+        vHtml += '<div class="sku_item_d"><a class="del_sku red_txt pointer block" onclick="btnDeleteOption();">删除</a></div>';
+        vHtml += '</li>';
+    });
+
+    $('#sku_ul').append(vHtml);
+}
+
+//商品添加页面输入项配置
+function GetInputConfig() {
+    //输入项相关配置
+    var InputConfig = {
+        '#uPhotoList li': { itemLength: { '>=': 2 }, name: '商品相关图片' },
+        '#txtTitle': { require: true, name: '商品名称' },
+        '#txtPrice': { require: true, name: '商品价格' },
+        '#selModule': { require: true, name: '类别' },
+        '#txtStock': { require: true, name: '库存' },
+        '#hCommIntro': { require: true, name: '商品介绍' }
+    };
+
+    //1.根据快递方式：0：免邮、1：自定义费用
+    var vPostFlag = $('#hPostFlag').val();
+
+    //2.根据有无商品规格：0：无规格、1：有规格
+    var vOptionFlag = $('#hCommOptionFlag').val();
+
+    //判断
+    if (vPostFlag == '1') {
+        InputConfig['#txtPostPrice'] = { require: true, name: '快递指定费用' };
+    }
+
+    if (vOptionFlag == '1') {
+        InputConfig['.sku_li div input'] = { dataType: 'list', require: true, name: '商品规格相关内容' };
+    }
+
+    return InputConfig;
+}
+
+//事件绑定方法公共
+function BindEventCommon() {
+    //2.取消按钮事件
+    $('#btnCancel').click(function () {
+        window.location.href = '/Admin/CommodityList';
+        return false;
+    });
+
+    //3.运费计算方式更改
+    $('#ckPostType').on('click', function () {
+        var vThis = $(this);
+
+        var vInput = $('#txtPostPrice');
+
+        if (this.checked) {
+            //置灰 + 给hidden赋值
+            $('#hPostFlag').val('0');
+            vInput.attr('disabled', 'disabled');
+
+        }
+        else {
+            $('#hPostFlag').val('1');
+            vInput.removeAttr('disabled');
+        }
+    });
+}
+
+//初始化展示数据公共
+function InitDataCommon() {
+    //1.商品类别数据加载
+    $.get('/RequestData/ShopModuleList',
+        { r: Math.random() },
+        function (data) {
+            if (data) {
+                var vHtml = '';
+                $.each(data, function (index, item) {
+                    vHtml += TxtFormat('<option value="{1}">{0}</option>', [item.Text, item.Value]);
+                });
+                $('#selModule').append(vHtml);
+            }
+        },
+        'json');
+}
--- StaticContent/js/Service/inputValidate.js
+++ StaticContent/js/Service/inputValidate.js
@@ -102,6 +102,7 @@ function InputValidate() {
     }
 }
 
+//用户输入错误提示用Alert来进行
 function InputValidateCNa() {
 
     //继承
@@ -112,5 +113,145 @@ function InputValidateCNa() {
         if (!flag) {
             alert(vMsg);
         }
+    }
+}
+
+//此版本支持元素数组对象
+function InputValidateCNb() {
+
+    //继承
+    InputValidateCNa.apply(this, arguments);
+
+    //提交数据验证方法
+    this.CheckInput = function (InputConfig, vClass) {
+        //Result
+        var vFlag = true;
+
+        $.each(InputConfig, function (index, item) {
+            //提取对象
+            var vItem = $(index);
+
+            //判断数据类型：是否需要循环
+            if (item["dataType"]) {
+                var vDataType = item["dataType"];
+
+                switch (vDataType) {
+                    case 'list': //元素数组
+                        $.each(vItem, function (cIndex, cItem) {
+                            if (!vClass.CkeckInputOpe(item, $(cItem), vClass)) {
+                                vFlag = false;
+                                return false;
+                            }
+                        });
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            else {
+                var vResult = vClass.CkeckInputOpe(item, vItem, vClass);
+
+                if (!vResult) {
+                    vFlag = false;
+                    return false;
+                }
+            }
+        });
+
+        return vFlag;
+    }
+
+    //进一步封装：支持数组元素对象
+    this.CkeckInputOpe = function (item, vItem, vClass) {
+        //Init
+        var vFlag = true;
+        var vMsg = '';
+        var vVal = vItem.val();
+
+        //验证第一段：为空
+        if (item['require']) {
+            if (!vVal || vVal.trim() == '') {
+                vMsg = item['name'] + '不能为空！';
+
+                //错误提示
+                vClass.ErrorTips(vItem, vMsg);
+
+                vFlag = false;
+                return vFlag;
+            }
+            else {
+                //隐藏错误提示
+                vClass.ErrorTips(vItem, '', 'hide');
+            }
+
+            vVal = vVal.trim();
+            vItem.val(vVal);
+        }
+
+        //第三段：指定项相同
+        if (item['equals']) {
+            var vEq = $('#' + item['equals']);
+            if (vVal != vEq.val().trim()) {
+                vMsg = TxtFormat('{0}与{1}输入内容不相同！', [item['name'], InputConfig[item['equals'].name]]);
+
+                //错误提示
+                vClass.ErrorTips(vItem, vMsg);
+
+                vFlag = false;
+                return vFlag;
+            }
+            else {
+                //隐藏错误提示
+                vClass.ErrorTips(vItem, '', 'hide');
+            }
+        }
+
+        //第二段：长度
+        if (item['length']) {
+            var vLength = item['length'];
+            if (vVal.length != vLength) {
+                vMsg = TxtFormat('请输入正确的{0}!', [item['name']]);
+
+                //错误提示
+                vClass.ErrorTips(vItem, vMsg);
+
+                vFlag = false;
+                return vFlag;
+            }
+            else {
+                //隐藏错误提示
+                vClass.ErrorTips(vItem, '', 'hide');
+            }
+        }
+
+        if (item['itemLength']) {
+            //Init
+            var vLengthConfig = item['itemLength'];
+            var vItemLength = vItem.length;
+
+            $.each(vLengthConfig, function (cIndex, cItem) {
+                switch (cIndex) {
+                    case '>=':
+                        if (!(vItemLength >= cItem)) {
+                            //错误提示
+                            vMsg = TxtFormat('{0}个数请多于{1}!', [item['name'], cItem]);
+                            vClass.ErrorTips(vItem, vMsg);
+
+                            vFlag = false;
+                            return vFlag;
+                        }
+                        else {
+                            //隐藏错误提示
+                            vClass.ErrorTips(vItem, '', 'hide');
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+        }
+        return vFlag;
     }
 }
